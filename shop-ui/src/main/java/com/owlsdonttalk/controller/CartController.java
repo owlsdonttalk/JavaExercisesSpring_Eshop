5902ee3133dd0303bb02b3fb37df09cf79088b0e
Ivan Petrov <petrovodt@gmail.com>
2021-04-19 21:16:27 +0300

    minor thymlead refactoring

--- shop-ui/src/main/java/com/owlsdonttalk/controller/CartController.java
+++ shop-ui/src/main/java/com/owlsdonttalk/controller/CartController.java
@@ -1,13 +1,42 @@
 package com.owlsdonttalk.controller;
 
+import com.owlsdonttalk.service.CategoryRepr;
+import com.owlsdonttalk.service.CategoryService;
+import com.owlsdonttalk.service.ProductRepr;
+import com.owlsdonttalk.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/cart")
 public class CartController {
 
     private static final Logger logger = LoggerFactory.getLogger(CartController.class);
+
+    private final ProductService productService;
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CartController(ProductService productService, CategoryService categoryService) {
+        this.productService = productService;
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/")
+    public String cartPage(Model model) {
+        logger.info("Shopping cart page request");
+        List<CategoryRepr> categories = categoryService.findAll();
+        model.addAttribute("categories", categories);
+        return "cart";
+    }
+
 
 
 }
